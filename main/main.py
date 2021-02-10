cd708e5aebc08b9cc89adde8f4f6b51618bc5573
Maciej Smukowski <msmukowski@gmail.com>
2021-02-10 23:59:10 +0100

    Added main function

--- main/main.py
+++ main/main.py
@@ -1,13 +1,17 @@
 from helpers import Target
 
 
-obj = Target()
+def main():
+    obj = Target()
 
-while obj.run:
-    obj.update()
+    while obj.run:
+        obj.update()
 
-    if obj.key_cap == ord('q'):
-        obj.cleanup()
-    else:
-        obj.display(obj.result)
+        if obj.key_cap == ord('q'):
+            obj.cleanup()
+        else:
+            obj.display(obj.result)
 
+
+if __name__ == '__main__':
+    main()
